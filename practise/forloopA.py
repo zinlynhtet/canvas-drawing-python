7614137cc1cd2e5b520e33fa1e2415745fa7a976
ZinLinnhtet <mackk5504@gmail.com>
2025-09-16 22:30:28 +0700

    loop

--- practise/forloopA.py
+++ practise/forloopA.py
@@ -15,10 +15,22 @@ while n > 0:
     print()
     n -= 1
 
-
 n = 5
 while n > 0:
     for i in range(n):
         print(i, end=' ')
     print()
     n -= 1
+    
+for j in range(10):
+    print("Line:", j, end=': ')
+    for i in range(j+1):
+        print(i, end=' ')
+    print()
+
+while True:
+    s = input("Enter your user name:").strip()
+    if s == "":  # empty string
+        break
+    print("Hello", s)
+print("Done")
